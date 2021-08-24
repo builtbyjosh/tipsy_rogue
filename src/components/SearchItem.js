69f604b95931ec55bfe5abb5d01acd2267fff77b
builtbyjosh <pc.refuge@gmail.com>
2021-08-24 16:55:32 -0400

    added semantic ui css

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const SearchItem = ({ result, setSelected }) => {
-  return <li key={result.id} onClick={() => setSelected(result)} style={{cursor: 'pointer'}} >{result.name}</li>;
+  return (
+    <li
+      key={result.id}
+      onClick={() => setSelected(result)}
+      style={{ cursor: 'pointer' }}
+    >
+      {result.name}
+    </li>
+  );
 };
 
 export default SearchItem;
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -8,14 +8,25 @@ const SearchList = ({ results }) => {
   return (
     <div>
       <h3>Results - Child 2</h3>
-
-      <ul>
-        {results.map((result) => (
-          <SearchItem result={result} setSelected={setSelected} />
-        ))}
-      </ul>
-
-      <SearchItemDetails selected={selected} />
+      <div className="ui segment">
+        <div className="ui two column very relaxed grid">
+          <div className="column">
+          <h3 className="ui left floated header">Details</h3>
+          <div className="ui clearing divider"></div>
+            <ul>
+              {results.map((result) => (
+                <SearchItem result={result} setSelected={setSelected} />
+              ))}
+            </ul>
+          </div>
+          <div className="column">
+            <h3 className="ui right floated header">Details</h3>
+            <div className="ui clearing divider"></div>
+            <SearchItemDetails selected={selected} />
+          </div>
+        </div>
+        <div className="ui vertical divider"></div>
+      </div>
     </div>
   );
 };
