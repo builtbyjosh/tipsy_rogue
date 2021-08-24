6b21fe3a85ebd81147ab53d3fa71b6fe1f655b24
builtbyjosh <pc.refuge@gmail.com>
2021-08-24 10:40:44 -0400

    added h1 to main display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 import Login from './components/forms/Login';
 import SignUp from './components/forms/SignUp';
 
 function App() {
   return (
-    <div className='ui container'>
+    <div className="ui container">
+      <h1>Tipsy Rogue</h1>
       <Login />
       <SignUp />
     </div>
